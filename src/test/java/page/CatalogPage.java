aa39de9f4af220d6c02502f3f31d4c7b33222149
V.Yermakovich <erviktoria19@gmail.com>
2021-06-16 18:26:43 +0300

    Fixed locator

--- src/test/java/page/CatalogPage.java
+++ src/test/java/page/CatalogPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.By;
 public class CatalogPage extends BasePage {
     private static final String CATALOG_NAVIGATION = "//span[contains(@class,'catalog-navigation-classifier__item-title-wrapper')][contains(text(),'%s')]"; //электрика
     private Button CATALOG_LIST =new Button(By.xpath("//div[@data-id='1']//div[contains(@class,'catalog-navigation-list__aside-list')]"));
-    private static final String LIST_TITLE_XPATH = "//a[@class='catalog-navigation-list__dropdown-item'] //..//..//..//../div[@class='catalog-navigation-list__aside-item']/div[@class='catalog-navigation-list__aside-title'][contains(text(),'%s')]";
+    private static final String LIST_TITLE_XPATH = "//a[@class='catalog-navigation-list__dropdown-item']/ancestor::div[@class='catalog-navigation-list__aside-item']/div[@class='catalog-navigation-list__aside-title'][contains(text(),'%s')]";
     public static final String LIST_DROPDOWN_XPATH = "//a[contains(@class,'catalog-navigation-list__dropdown-item')]//span[contains(text(),'%s')]";
     public static final String WAIT_FOR_IS_PAGE_OPEN = "//h1[contains(@class,'catalog-navigation__title')][contains(text(),'%s')]";
 
